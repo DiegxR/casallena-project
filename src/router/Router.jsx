c2aa52acd6dbc5c9b48828ff7690b77c3bd87603
Pangie <angierincon326@gmail.com>
2023-04-13 16:27:59 -0500

    register
    git commit -m register

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AppStar from "../components/appStar/AppStar";
 import Inicio from "../home/inicioStart/Inicio";
+import Register from "../login/register/Register";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Inicio />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
