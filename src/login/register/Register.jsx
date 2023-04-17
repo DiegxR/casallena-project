aa6d4826ffd3eb99f73c8ced04066084c2d1fff3
Esteark <eardila1409@gmail.com>
2023-04-17 08:00:47 -0500

    resolving changes upload Photo

--- src/login/register/Register.jsx
+++ src/login/register/Register.jsx
@@ -2,69 +2,78 @@ import React, { useEffect, useState } from "react";
 import "./register.scss";
 import { AiOutlineArrowLeft } from "react-icons/ai";
 import { useNavigate } from "react-router";
-import { motion } from 'framer-motion'
+import { motion } from "framer-motion";
 import { useForm } from "react-hook-form";
 import { useDispatch, useSelector } from "react-redux";
-import { createUserWithEmail, toggleError } from "../../redux/actions/userActions";
-import Loading from '../../components/loading/Loading'
+import {
+  createUserWithEmail,
+  toggleError,
+} from "../../redux/actions/userActions";
+import Loading from "../../components/loading/Loading";
 import { notify } from "../../services/notify";
-const minPass = /^.{6,}$/
-const upperPass = /[A-Z]/
-const specialPass = /[\W_]/
+const minPass = /^.{6,}$/;
+const upperPass = /[A-Z]/;
+const specialPass = /[\W_]/;
 const Register = () => {
-  const navigate = useNavigate()
-  const [passWord, setPassWord] = useState('')
-  const [errorPass, setErrorPass] = useState('')
-  const validatePass = (value) =>{
-    console.log(value)
-    if(!minPass.test(value)){
-      setErrorPass('Debe contener mínimo 6 dígitos')
-    }else if(!upperPass.test(value)){
-      setErrorPass('También debe contener mínimo una mayúscula')
-    }else if(!specialPass.test(value)){
-      setErrorPass('También debe contener un carácter especial')
-    }else{
-      setErrorPass('')
-      setPassWord(value)
+  const navigate = useNavigate();
+  const [passWord, setPassWord] = useState("");
+  const [errorPass, setErrorPass] = useState("");
+  const validatePass = (value) => {
+    console.log(value);
+    if (!minPass.test(value)) {
+      setErrorPass("Debe contener mínimo 6 dígitos");
+    } else if (!upperPass.test(value)) {
+      setErrorPass("También debe contener mínimo una mayúscula");
+    } else if (!specialPass.test(value)) {
+      setErrorPass("También debe contener un carácter especial");
+    } else {
+      setErrorPass("");
+      setPassWord(value);
     }
-  }
-  
-  const validatePasswords = (passWordConf) =>{
-    if(passWord !== passWordConf){
-      console.log('errorr')
-      setError('passwordConf', {message: 'Las contraseñas no coinciden'})
-    }else{
-      setError('passwordConf', {message: ''})
+  };
+
+  const validatePasswords = (passWordConf) => {
+    if (passWord !== passWordConf) {
+      console.log("errorr");
+      setError("passwordConf", { message: "Las contraseñas no coinciden" });
+    } else {
+      setError("passwordConf", { message: "" });
     }
-  }
- const {error, loading} = useSelector(store => store.user)
-  const {register, handleSubmit, formState: {errors}, setError} = useForm()
-  const dispatch = useDispatch()
-  const onSubmit = (data) =>{
-    dispatch(createUserWithEmail(data))
-  }
+  };
+  const { error, loading } = useSelector((store) => store.user);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm();
+  const dispatch = useDispatch();
+  const onSubmit = (data) => {
+    dispatch(createUserWithEmail(data));
+  };
   useEffect(() => {
-    if(error.status === true){
-      notify(error.message, '#d80416', '#d80416')
-    }else if(error.status === false){
-      notify('Ususario creado exitosamente', '#027087', '#d80416')
-      dispatch(toggleError())
-      navigate('/photouser')
+    if (error.status === true) {
+      notify(error.message, "#d80416", "#d80416");
+    } else if (error.status === false) {
+      notify("Ususario creado exitosamente", "#027087", "#027087");
+      dispatch(toggleError());
+      navigate("/photouser");
     }
-  }, [error])
-  
+  }, [error]);
+
   return (
     <motion.section
-      initial={{ x: '100%' }}
-      transition={{ duration: 1, ease: 'easeIn' }}
-      exit={{x: window.innerWidth}}
-      animate={{ x: '0%' }}
-      className="registerSec">
+      initial={{ x: "100%" }}
+      transition={{ duration: 1, ease: "easeIn" }}
+      exit={{ x: window.innerWidth }}
+      animate={{ x: "0%" }}
+      className="registerSec"
+    >
       <AiOutlineArrowLeft onClick={() => navigate(-1)} className="arrowLeft" />
       <form onSubmit={handleSubmit(onSubmit)} className="registerSec__form">
         <label htmlFor="" className="registerSec__label">
           <input
-          {...register('email', {required: 'El email es requerido'})}
+            {...register("email", { required: "El email es requerido" })}
             type="email"
             className="registerSec__input"
             placeholder="Email"
@@ -75,7 +84,7 @@ const Register = () => {
 
         <label htmlFor="" className="registerSec__label">
           <input
-          {...register('location', {required: ''})}
+            {...register("location", { required: "" })}
             type="text"
             className="registerSec__input"
             placeholder="Ciudad"
@@ -86,7 +95,7 @@ const Register = () => {
 
         <label htmlFor="" className="registerSec__label">
           <input
-          {...register('name', {required: ''})}
+            {...register("name", { required: "" })}
             type="text"
             className="registerSec__input"
             placeholder="Nombre de usuario"
@@ -97,29 +106,35 @@ const Register = () => {
 
         <label htmlFor="" className="registerSec__label">
           <input
-            onInput={(e)=>validatePass(e.target.value)}
+            onInput={(e) => validatePass(e.target.value)}
             type="password"
             className="registerSec__input"
             placeholder="Contraseña"
           />
-          {errorPass !== '' ? <span>{errorPass}</span> : ''}
+          {errorPass !== "" ? <span>{errorPass}</span> : ""}
           Contraseña
         </label>
         <label htmlFor="" className="registerSec__label">
           <input
-          {...register('passwordConf', {required: ''})}
+            {...register("passwordConf", { required: "" })}
             type="password"
             className="registerSec__input"
             placeholder="Confirmar contraseña"
-            onInput={e => validatePasswords(e.target.value)}
+            onInput={(e) => validatePasswords(e.target.value)}
           />
           Confirmar contraseña
-          {errors.passwordConf ? <span>{errors.passwordConf.message}</span> : <></>}
+          {errors.passwordConf ? (
+            <span>{errors.passwordConf.message}</span>
+          ) : (
+            <></>
+          )}
         </label>
 
-        <button type="submit" className="registerSec__btn">CREAR CUENTA</button>
+        <button type="submit" className="registerSec__btn">
+          CREAR CUENTA
+        </button>
       </form>
-      {loading ? <Loading/> : <></>}
+      {loading ? <Loading /> : <></>}
     </motion.section>
   );
 };
